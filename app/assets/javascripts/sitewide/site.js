27fb610dd6d2cd2670a7031fb39c32c9ad1a4624
Sahash <sahash182@gmail.com>
2015-11-05 17:30:31 -0800

    simple fixes on form for user

--- app/assets/javascripts/sitewide/site.js
+++ app/assets/javascripts/sitewide/site.js
@@ -1,7 +1,22 @@
 $(function(){
   
 //hiding the button
-$(".btn-default").hide();
+$(function(){
+    $('input[name="rad"]').click(function(){
+        var $radio = $(this);
 
+        // if this was previously checked
+        if ($radio.data('waschecked') == true)
+        {
+            $radio.prop('checked', false);
+            $radio.data('waschecked', false);
+        }
+        else
+            $radio.data('waschecked', true);
+
+        // remove was checked from other radios
+        $radio.siblings('input[name="rad"]').data('waschecked', false);
+    });
+});
 
 });
