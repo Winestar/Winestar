e73eb5427ce4e73ea935ae39e5d50d3161a0e5f1
rchau05 <rnchau@mail.sfsu.edu>
2015-11-07 00:10:05 -0800

    added PMI email addresses

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,12 +4,7 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    mail(:to => user.email, :subject => "Thank you for signing up with Winestar!", :bcc => ['tlpride415@gmail.com'])
-  end
-
-  def trip_info(trip)
-  	@trip = trip
-  	mail(:to => user.email, :subject => "Thank you for signing up with Winestar!", :bcc => ['tlpride415@gmail.com'])
+    mail(:to => user.email, :subject => "Thank you for signing up with Winestar!", :bcc => ['tlpride415@gmail.com', 'Julie.Vaughn@gmail.com', 'joeruef@gmail.com', 'kshah3@babson.edu', 'leogarver@gmail.com'])
   end
 
 end
