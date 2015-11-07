d6491a439bb96f0e6e5a17ac066bf9e4a9376872
Sahash <sahash182@gmail.com>
2015-11-06 19:48:57 -0800

    images done for preferences

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ActiveRecord::Base
-	# has_many :trips, dependent: :destroy
-	# has_one :like, dependent: :destroy
+	has_many :trips, dependent: :destroy
+	has_one :like, dependent: :destroy
 	has_secure_password
-
-  
 end
