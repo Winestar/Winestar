109e09ed97bf8be413ad8824384902605ce71d48
Sahash <sahash182@gmail.com>
2015-11-06 16:17:38 -0800

    preference page with pictures

--- app/assets/javascripts/sitewide/site.js
+++ app/assets/javascripts/sitewide/site.js
@@ -1,22 +1,6 @@
 $(function(){
   
 //hiding the button
-$(function(){
-    $('input[name="rad"]').click(function(){
-        var $radio = $(this);
 
-        // if this was previously checked
-        if ($radio.data('waschecked') == true)
-        {
-            $radio.prop('checked', false);
-            $radio.data('waschecked', false);
-        }
-        else
-            $radio.data('waschecked', true);
-
-        // remove was checked from other radios
-        $radio.siblings('input[name="rad"]').data('waschecked', false);
-    });
-});
 
 });
