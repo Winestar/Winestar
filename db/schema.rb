da19d88edaa76a1b9d5246bf9d42fc3135ea6719
rchau05 <rnchau@mail.sfsu.edu>
2015-11-09 14:18:24 -0800

    fixing db errors.  pulling from working version from github

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109103608) do
+ActiveRecord::Schema.define(version: 20151109100229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20151109103608) do
     t.string   "kid_friendly"
     t.string   "dog_friendly"
     t.string   "bachelorettes"
+    t.string   "picnics"
     t.string   "staff_picks"
     t.string   "organic_wineries"
     t.string   "architecture"
@@ -32,13 +33,9 @@ ActiveRecord::Schema.define(version: 20151109103608) do
     t.string   "top_whites"
     t.string   "sparkling_wines"
     t.string   "dessert_wines"
-    t.string   "picnic"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
-    t.integer  "user_id"
   end
-
-  add_index "likes", ["user_id"], name: "index_likes_on_user_id", using: :btree
 
   create_table "trips", force: :cascade do |t|
     t.boolean  "available_now"
@@ -50,12 +47,6 @@ ActiveRecord::Schema.define(version: 20151109103608) do
     t.integer  "price_range"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
-  end
-
-  create_table "userpicks", force: :cascade do |t|
-    t.integer "userpick"
-    t.integer "like_id"
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
