6af6b4d2012dd0d5693f2b105395bc9295d2de37
Sahash <sahash182@gmail.com>
2015-11-10 23:35:47 -0800

    added styling to splash page

--- app/assets/javascripts/sitewide/site.js
+++ app/assets/javascripts/sitewide/site.js
@@ -1,32 +1,14 @@
 $(function(){
   
-
-//   judge.validate(document.getElementById('val'), {
-//   valid: function(element) {
-//     element.style.border = '1px solid green';
-//   },
-//   invalid: function(element, messages) {
-//     element.style.border = '1px solid red';
-//     alert(messages.join(','));
-//   }
-// })
-
-//   judge.validate(document.getElementById('email'), {
-//   valid: function(element) {
-//     element.style.border = '1px solid green';
-//   },
-//   invalid: function(element, messages) {
-//     element.style.border = '1px solid red';
-//     alert(messages.join(','));
-//   }
-// });
   
-//hiding the button
-$('#next').click(function() {
-if(($('input[name=like[vineyard_tours]]:checked').length>4) || ($('input[name=like[vineyard_tours]]:checked').length<1))
-{
-alert("Please Select Up To 4 that you prefer")
-event.preventDefault();
-}
-})
+    //user can only select up to four fields from the like model
+    $('#next').click(function() {
+      if(($('input[name=like[vineyard_tours]]:checked').length>4) || ($('input[name=like[vineyard_tours]]:checked').length<1))
+        {
+          alert("Please Select Up To 4 that you prefer")
+          event.preventDefault();
+        }
+    })
+
+
 });
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,31 +1,14 @@
 class LikesController < ApplicationController
  
- # def index
- #   @likes = Like.all
- # end
-
- # # def show
- # #   @like = Like.find(params[:id])
- # # end
-
- # form to create like
- # def new
- #   @like = current_user.likes.new
- #   # @user = user.find(params)
- #   render :new
- # end
  
  # creates new likes into db
  def create
    @like = current_user.likes.create!(like_params)
    
-
    respond_to do |format|
+
      if @like.save
-
-       # flash[:notice] = "You Signed up successfully"
-
-       # Tell the UserMailer to send a welcome email after save
+       # Tell the UserMailer to send a like info emailed after save
        UserMailer.like_email(@like).deliver
 
         format.html { redirect_to trips_path}
@@ -35,30 +18,7 @@ class LikesController < ApplicationController
         format.json { render json: @like.errors, status: :unprocessable_entity }
       end
     end
-
-
-   # respond_to do |format|
-   #   if @like.save
-   #     # Tell the UserMailer to send a welcome email after save
-   #     UserMailer.like_email(@like).deliver
-
-   #     format.json { render :show, status: :created, location: @like }
-   #   else
-   #     format.html { redirect_to trips_path}
-   #     format.json { render json: @like.errors, status: :unprocessable_entity }
-   #   end
-   # end
-   # if @like.save
-   #   # @user = current_user
-   #   # redirected_to @user, notice: "Signed up successfully."
-   #   # session[:user_id] = user.user_id
-   #   redirect_to trips_path
-   # else
-     # format.html { render action: "new"}
-     # redirect_to trips_path
-   # end
- end
-
+  end
 
  private
  def like_params
