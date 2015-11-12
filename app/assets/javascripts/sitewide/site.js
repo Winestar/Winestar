d779ab8aa1d00cfb91b22ed3f8612f2181b41c19
Sahash <sahash182@gmail.com>
2015-11-12 02:00:44 -0800

    routes fixed

--- app/assets/javascripts/sitewide/site.js
+++ app/assets/javascripts/sitewide/site.js
@@ -2,7 +2,6 @@ $(function(){
   
   //jquery validation for the trips form
   $('#trip-submit').click(function(){
-
     if(
         ($('#trip_arrivals_date').val() === '') || 
         ($('#trip_departure_date').val() === '') ||
@@ -14,20 +13,8 @@ $(function(){
         alert('Please fill out all the fields for your trip.')
         event.preventDefault();
       }
-    // if ($('#trip_arrivals_date').val() && $('#trip_departure_date').val() &&
-    //   $('#origin-input').val() && $('#destination-input').val() && 
-    // if ($('#trip_additional_info').val() === "" )
-    //  {
-    //     alert('Please fill out all the fields for your trip.');
-    // }
-    // else if ($('#trip_arrivals_date').val() === "") {
-
-    // }
   })
 
-
-
-  
     //user can only select up to four fields from the like model
     $('#next').click(function() {
       if(($('input[name=like[vineyard_tours]]:checked').length>4) || ($('input[name=like[vineyard_tours]]:checked').length<1))
